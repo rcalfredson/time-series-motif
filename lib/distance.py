2cf35bc9152050c6f5f53bb0043813320d2a6bda
Robert C. Alfredson <robert.c.alfredson@gmail.com>
2021-12-13 21:42:03 -0500

    Update docstring for distance function

--- lib/distance.py
+++ lib/distance.py
@@ -4,7 +4,7 @@ import numpy as np
 def distance(a: np.ndarray, b: np.ndarray):
     """compute Euclidean distance matrix
 
-    adapted from code written by Roland Bunschoten,
+    adapted from Matlab code written by Roland Bunschoten,
     University of Amsterdam
     """
     a, b = [np.expand_dims(arr, 0) if arr.ndim == 1 else arr for arr in (a, b)]
